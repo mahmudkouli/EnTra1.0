88b50452277e64f586a31eaf969e97390471a509
Mahmud Koulizade <32882494+mahmudkouli@users.noreply.github.com>
2020-01-05 17:07:26 -0500

    Added attachments to the email object
    Now email object has attachments with the technical indicator plots

--- source_code/email_engine.py
+++ source_code/email_engine.py
@@ -1,15 +1,45 @@
 import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import os 
+from email.mime.base import MIMEBase
+from email import encoders
 
 def email_object(data, user_name):
-	msg = MIMEMultipart()
+    msg = MIMEMultipart()
+    
+    attachments = os.listdir('../daily_reports')
+    
+    # open the file to be sent
+    
+    for file in attachments:
+            
+        print(file)
+        attachment = open('../daily_reports/'+file, "rb")
+    
+        # instance of MIMEBase and named as p
+        p = MIMEBase('application', 'octet-stream')
+        
+        # To change the payload into encoded form
+        p.set_payload((attachment).read())
+        
+        # encode into base64
+        encoders.encode_base64(p)
+        
+        p.add_header('Content-Disposition', "attachment; filename= %s" % file)
+        
+        # attach the instance 'p' to instance 'msg'        
+        msg.attach(p)
 
-	msg['From']= user_name
-	msg['To']= 'mahmudkouli@gmail.com'
-	#msg['CC']='ksenia.ter@gmail.com'
-	msg['Subject']="TESTING: Entra Alert, " + str(datetime.date.today())
+    msg['From']= user_name
+    msg['To']= 'mahmudkouli@gmail.com'
+    #msg['CC']='ksenia.ter@gmail.com'
+    msg['Subject']="TESTING: Entra Alert, " + str(datetime.date.today())
 
-	msg.attach(MIMEText(data.to_html(), 'html'))
+    msg.attach(MIMEText(data.to_html(), 'html'))
 
-	return msg
+    return msg
+
+
+
+
